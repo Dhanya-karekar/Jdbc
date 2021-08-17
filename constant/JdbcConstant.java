bfca34f94bbdce686d65820c7b16389d24d40bab
Dhanya-karekar <85379916+Dhanya-karekar@users.noreply.github.com>
2021-08-17 12:10:20 +0530

    Add files via upload

---
+++ constant/JdbcConstant.java
@@ -0,0 +1,9 @@
+package com.xworkz.jdbc.customer.constant;
+
+public class JdbcConstant {
+
+	public static final String user = "root";
+	public static final String password = "4PM15IS@011";
+	public static final String url = "jdbc:mysql://127.0.0.1:3306/webseries";
+
+}
